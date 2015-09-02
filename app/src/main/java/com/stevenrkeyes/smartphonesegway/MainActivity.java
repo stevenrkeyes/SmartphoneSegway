5ae7e48d9d9eef7417bdf8fb8836bbae23f785df
stevenrkeyes <stevenrkeyes@gmail.com>
2015-09-01 18:34:55 -0700

    got project to compile and read from IMU; currently displaying readings to screen for debugging

--- app/src/main/java/com/stevenrkeyes/smartphonesegway/MainActivity.java
+++ app/src/main/java/com/stevenrkeyes/smartphonesegway/MainActivity.java
@@ -1,16 +1,33 @@
 package com.stevenrkeyes.smartphonesegway;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    // handles for the IMU
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+
+    // for storing IMU data
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Initialize things related to the IMU
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -33,5 +50,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent e) {
+        // use the sensor event to check what kind of sensor it is
+        Sensor mySensor = e.sensor;
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = e.values[0];
+            float y = e.values[1];
+            float z = e.values[2];
+            ((android.widget.TextView) findViewById(R.id.x_reading)).setText(Float.toString(x));
+            ((android.widget.TextView) findViewById(R.id.y_reading)).setText(Float.toString(y));
+            ((android.widget.TextView) findViewById(R.id.z_reading)).setText(Float.toString(z));
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
