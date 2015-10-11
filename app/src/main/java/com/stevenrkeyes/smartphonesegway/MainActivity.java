6f270cbb34c79651d931ee1fc5ccec9c05a870b8
stevenrkeyes <stevenrkeyes@gmail.com>
2015-10-10 22:39:53 -0400

    added bluetooth stuff so I can now toggle a LED on an arduino with a button on my phone; need to commit arduino firmware

--- app/src/main/java/com/stevenrkeyes/smartphonesegway/MainActivity.java
+++ app/src/main/java/com/stevenrkeyes/smartphonesegway/MainActivity.java
@@ -1,6 +1,10 @@
 package com.stevenrkeyes.smartphonesegway;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -13,6 +17,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     // handles for the IMU
@@ -22,6 +30,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     // for storing IMU data
     private long lastUpdate = 0;
     private float last_x, last_y, last_z;
+
+    // options for bluetooth
+    private static final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
+    private static final UUID my_uuid = UUID.fromString("0000000-0000-1000-8000-00805f9b34fb");
+    private static String address = "20:15:04:15:80:55";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +52,121 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         final Button bluetooth_button = (Button) findViewById(R.id.bluetooth_button);
         bluetooth_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // perform action on click
+                // send bluetooth stuff
+                sendData("a");
+
+                // toast to indicate stuff happened
                 Toast toast = Toast.makeText(getApplicationContext(), "It works!", Toast.LENGTH_SHORT);
                 toast.show();
             }
         });
+
+        // initilialize stuff for bluetooth
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        checkBTState();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // On resume, reconnect to the bluetooth device
+        // Set up a pointer to the remote node using its address
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        // Both the MAC address (acquired above) and Service ID (UUID) are required for the connection
+        try {
+            btSocket = device.createRfcommSocketToServiceRecord(my_uuid);
+        } catch (IOException e) {
+            errorExit("Fata Error", "In onResume(), socket create failed: " + e.getMessage() + ".");
+        }
+
+        // turn of discovery because it's resource intensive
+        btAdapter.cancelDiscovery();
+
+        // Establish the connection
+        try {
+            btSocket.connect();
+            Toast toast = Toast.makeText(getApplicationContext(), "Socket Connected", Toast.LENGTH_SHORT);
+            toast.show();
+        } catch (IOException e) {
+            try {
+                // for android >4.2 or something, the bluetooth is a little different
+                // or requires some kind of fallback here, i don't really get it
+                btSocket = (BluetoothSocket) device.getClass().getMethod("createRfcommSocket", new Class[]{int.class}).invoke(device, 1);
+                btSocket.connect();
+            } catch (Exception e2) {
+                try {
+                    btSocket.close();
+                    Toast toast = Toast.makeText(getApplicationContext(), e2.getMessage(), Toast.LENGTH_SHORT);
+                    toast.show();
+                } catch (IOException e3) {
+                    errorExit("Fatal Error", "In onResume, unable to close socket during connection failure: " + e3.getMessage() + ".");
+                }
+            }
+        }
+
+        try {
+            outStream = btSocket.getOutputStream();
+        } catch (IOException e) {
+            errorExit("Fata Error", "In onResume(), output stream creation failed: " + e.getMessage() + ".");
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // close the bluetooth when the app is paused
+
+        if (outStream != null) {
+            try {
+                outStream.flush();
+            } catch (IOException e) {
+                errorExit("Fatal Error", "In onPause(), failed to flush output stream: " + e.getMessage() + ".");
+            }
+        }
+
+        try {
+            btSocket.close();
+        } catch (IOException e2) {
+            errorExit("Fatal Error", "In onPause(), failed to close socket." + e2.getMessage() + ".");
+        }
+    }
+
+    private void checkBTState() {
+        // Check for Bluetooth support and then check to make sure it is turned on
+
+        // Emulator doesn't support Bluetooth and will return null
+        if (btAdapter == null) {
+            errorExit("Fatal Error", "Bluetooth Not supported. Aborting.");
+        } else {
+            if (btAdapter.isEnabled()) {
+            } else {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    private void errorExit(String title, String message) {
+        Toast msg = Toast.makeText(getBaseContext(),
+                title + " - " + message, Toast.LENGTH_LONG);
+        msg.show();
+        finish();
+    }
+
+    private void sendData(String message) {
+        byte[] msgBuffer = message.getBytes();
+
+        try {
+            outStream.write(msgBuffer);
+        } catch (IOException e) {
+            String msg = "In onResume(), an exception occurred during write: " + e.getMessage();
+            msg = msg + ".\n\nCheck that the SPP UUID: " + my_uuid.toString() + " exists on server.\n\n";
+            errorExit("Fatal Error", msg);
+        }
     }
 
     @Override
