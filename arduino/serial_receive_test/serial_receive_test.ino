9b1bfeaf417a2bc11bc941125ab83f2f70b849c4
Steven <stevenrkeyes@gmail.com>
2015-10-20 13:51:25 -0400

    added servo control

--- arduino/serial_receive_test/serial_receive_test.ino
+++ arduino/serial_receive_test/serial_receive_test.ino
@@ -1,10 +1,17 @@
+#include <Servo.h>                          
+
 const int LED_pin = 13;
+
+Servo right_servo;
+Servo left_servo;
 
 void setup() 
 { 
   Serial.begin(9600);
   pinMode(LED_pin, OUTPUT);
   digitalWrite(LED_pin, LOW);
+  right_servo.attach(5);
+  left_servo.attach(3);
 } 
  
 void loop() 
@@ -13,9 +20,18 @@ void loop()
     int received_byte = Serial.read();
     if (received_byte == 'a') {
       digitalWrite(LED_pin, HIGH);
+      right_servo.write(90-15);
+      left_servo.write(90+15);
     }
     else if (received_byte == 'b') {
       digitalWrite(LED_pin, LOW);
+      right_servo.write(90+15);
+      left_servo.write(90-15);
+    }
+    else if (received_byte == 'c') {
+      digitalWrite(LED_pin, LOW);
+      right_servo.write(90);
+      left_servo.write(90);
     }
   }
 } 
