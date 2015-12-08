ac795d80146db5bc166a3f20febef4b90004079e
Steven <stevenrkeyes@gmail.com>
2015-12-08 04:36:21 -0500

    added a communications protocol into the arduino firmware; can now specify a servo speed, like 'a-2.8z' for -2.8rad/s

--- arduino/serial_receive_test/serial_receive_test.ino
+++ arduino/serial_receive_test/serial_receive_test.ino
@@ -5,6 +5,21 @@ const int LED_pin = 13;
 Servo right_servo;
 Servo left_servo;
 
+String receive_buffer = "";
+
+// set a speed in radians per second, with positive being forward
+void setServoSpeed(float radsPerSec)
+{
+  // constrain the values
+  radsPerSec = max(radsPerSec, -2.8);
+  radsPerSec = min(radsPerSec, 2.8);
+  // 15 corresponds to 2.8 rad/s
+  float servoValue = 15*radsPerSec/2.8;
+  int stoppedValue = 91;
+  right_servo.write(stoppedValue - (int)servoValue);
+  left_servo.write(stoppedValue + (int)servoValue);
+}
+
 void setup() 
 { 
   Serial.begin(9600);
@@ -19,19 +34,20 @@ void loop()
   if (Serial.available() > 0) {
     int received_byte = Serial.read();
     if (received_byte == 'a') {
-      digitalWrite(LED_pin, HIGH);
-      right_servo.write(90-15);
-      left_servo.write(90+15);
+      receive_buffer = "";
     }
-    else if (received_byte == 'b') {
-      digitalWrite(LED_pin, LOW);
-      right_servo.write(90+15);
-      left_servo.write(90-15);
+    else if (received_byte == 'z') {
+      // parse the buffer
+      float parsed_buffer = receive_buffer.toFloat();
+      setServoSpeed(parsed_buffer);
     }
     else if (received_byte == 'c') {
-      digitalWrite(LED_pin, LOW);
-      right_servo.write(90);
-      left_servo.write(90);
+      // clear the buffer (for debugging)
+      receive_buffer = "";
+      setServoSpeed(0);
+    }
+    else { // any other character is added to the buffer
+      receive_buffer += (char)received_byte;
     }
   }
 } 
